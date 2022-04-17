459d520c74dc4737c20b1a29aeefc80c815264d4
zdehkirill <73167768+school239top@users.noreply.github.com>
2022-04-17 20:27:12 +0300

    Add files via upload

---
+++ NMR_P_P.py
@@ -0,0 +1,9 @@
+
+# Задача P-13: " Преобразование многочлена — кратные корни в простые"
+# Здех К.В.
+
+def NMR_P_P(k,arr):
+    z,m = DER_P_P(k,arr)
+    u,t = GCF_PP_P(k,arr,z,m)
+    j,l = DIV_PP_P(u,t)
+    return [j,l]
